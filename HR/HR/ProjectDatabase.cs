350a941868dd64808d102b48281b37591d6945f1
torresjuanlucas <torresjuanlucas@gmail.com>
2017-10-05 10:14:03 -0400

    * project with ProjectDatabase.cs (class)

---
+++ HR/HR/ProjectDatabase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR
+{
+    class ProjectDatabase
+    {
+        private string connString = @"Server=tcp:lucastorres.database.windows.net,1433;Initial Catalog=hrproject;Persist Security Info=False;User ID={lucastorres};Password={LucasLucas1};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+    }
+}
