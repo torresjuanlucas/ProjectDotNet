c71b5a4b63fe4ff3fe751bca9275f0fc481b6032
torresjuanlucas <torresjuanlucas@gmail.com>
2017-10-10 14:05:01 -0400

    * added classes Lucas

---
+++ HR/HR/Employee.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR
+{
+    class Employee
+    {
+        private string _name;
+        private int _age;
+        private double _height;
+
+        public int Id { get; set; }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length < 2 || value.Length > 50)
+                {
+                    throw new ArgumentOutOfRangeException("Name must be between 2 and 50 characters long");
+                }
+                _name = value;
+            }
+        }
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value < 1 || value > 150)
+                {
+                    throw new ArgumentOutOfRangeException("Age must be between 1 and 150");
+                }
+                _age = value;
+            }
+        }
+        public string AgeStr
+        {
+            set
+            {
+                int ageTemp;
+                if (int.TryParse(value, out ageTemp))
+                {
+                    Age = ageTemp;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Age must be an integer value");
+                }
+            }
+        }
+
+
+        public double Height
+        {
+            get { return _height; }
+            set
+            {
+                if (value < 0.3 || value > 3)
+                {
+                    throw new ArgumentOutOfRangeException("Height must be between 0.3 and 3.0 meters");
+                }
+                _height = value;
+            }
+        }
+
+        public override String ToString()
+        {
+            return String.Format("{0}: {1} is {2} y/o, {3:F2}m tall", Id, Name, Age, Height);
+        }
+    }
+}
--- HR/HR/ProjectDatabase.cs
+++ HR/HR/ProjectDatabase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,55 @@ namespace HR
     class ProjectDatabase
     {
         private string connString = @"Server=tcp:lucastorres.database.windows.net,1433;Initial Catalog=hrproject;Persist Security Info=False;User ID={lucastorres};Password={LucasLucas1};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private SqlConnection conn;
+
+        public ProjectDatabase()
+        {
+            conn = new SqlConnection();
+            conn.ConnectionString = connString;
+            conn.Open();
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            List<Employee> result = new List<Employee>();
+            SqlCommand selectCommand = new SqlCommand("SELECT * FROM People ORDER BY Id", conn);
+            using (SqlDataReader reader = selectCommand.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    int xId = (int)reader["Id"];
+                    string xName = (string)reader["Name"];
+                    int xAge = (int)reader["Age"];
+                    double xHeight = (float)reader["Height"]; // float -> double
+                    Employee e = new Employee { Id = xId, Name = xName, Age = xAge, Height = xHeight };
+                    result.Add(e);
+                }
+            }
+            return result;
+        }
+
+        // NOTE add modified so that it returns ID of the record just created
+        public int AddEmployee(Employee e)
+        {
+            SqlCommand insertCommand = new SqlCommand("INSERT INTO People (Name, Age, Height) VALUES (@Name, @Age, @Height); SELECT SCOPE_IDENTITY();", conn);
+            insertCommand.Parameters.Add(new SqlParameter("Name", e.Name));
+            insertCommand.Parameters.Add(new SqlParameter("Age", e.Age));
+            insertCommand.Parameters.Add(new SqlParameter("Height",e.Height));
+            //insertCommand.ExecuteNonQuery();
+            int id = (int)insertCommand.ExecuteScalar(); // return ID of the record just created
+            return id;
+        }
+
+        public void DeleteEmployee(int Id)
+        {
+
+        }
+
+        public void UpdateEmployee(Employee e)
+        {
+
+        }
+
     }
 }
