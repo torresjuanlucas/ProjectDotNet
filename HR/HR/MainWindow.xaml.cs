337ecaca62d5f606c6e782eb403dfbd241c705fc
STUDENTS\1795643 <mozhanahmadi@hotmail.com>
2017-10-12 11:14:14 -0400

    * added update window

--- HR/HR/MainWindow.xaml.cs
+++ HR/HR/MainWindow.xaml.cs
@@ -40,14 +40,24 @@ namespace HR
         }
 
 
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateEmployee win2 = new UpdateEmployee();
+            win2.Show();
+            this.Close();
+        }
 
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Add_Click(object sender, RoutedEventArgs e)
         {
             AddEmployee win2 = new AddEmployee();
             win2.Show();
             this.Close();
         }
+
+        
+
+
+       
 
 
         private void ReloadEmployeeList()
@@ -59,6 +69,49 @@ namespace HR
                 lsvEmployee.Items.Add(E);
             }
         }
+
+
+
+
+        private void lsvEmployee_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            ContextMenu cm = this.FindResource("lsvEmployee") as ContextMenu;
+            cm.PlacementTarget = sender as ListView;
+            cm.IsOpen = true;
+        }
+
+        //private void lsvPeople_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        //{
+        //    List<Employee> Employees = new List<Employee>();
+        //    if (lsvEmployee.SelectedIndex != -1)
+        //    {
+        //        tbFullName.Text = Employees.ElementAt(lsvEmployee.SelectedIndex).FullName;
+        //        tbAge.Text = Employees.ElementAt(lsvEmployee.SelectedIndex).Age.ToString();
+        //        sliderHeight.Value = Convert.ToInt64(Employees.ElementAt(lsvEmployee.SelectedIndex).Height);
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("You must choose an Item. Just Cicking with out choosing does not work!", "My App", MessageBoxButton.OK, MessageBoxImage.Warning);
+        //    }
+
+
+        private void MenuItemDelete_Click(object sender, RoutedEventArgs e)
+        {
+            List<Employee> Persons = new List<Employee>();
+
+            if (lsvEmployee.SelectedIndex < 0 )
+            {
+                MessageBox.Show("You must add some data First then try to delete one!", "My App", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            Persons.RemoveAt(lsvEmployee.SelectedIndex);
+            lsvEmployee.Items.Refresh();
+        }
+
+       
+
+       
+    
     }
 }
       
