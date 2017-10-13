dbbc40ed74d91b18fb873e6d357c26ebe3f6ee38
STUDENTS\1795643 <mozhanahmadi@hotmail.com>
2017-10-13 15:31:57 -0400

    *Created Add payment - created tables- with the help of Vajihe

---
+++ HR/HR/AddPayment.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HR
+{
+    /// <summary>
+    /// Interaction logic for AddPayment.xaml
+    /// </summary>
+    public partial class AddPayment : Window
+    {
+        ProjectDatabase db;
+        public AddPayment()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            double hourlyRate;
+            if (!double.TryParse(tbHourlyRatePay.Text, out hourlyRate))
+            {
+                MessageBox.Show("error input");
+                return;
+            }
+            double hours;
+            if (!double.TryParse(tbNoOfHoursPay.Text, out hours))
+            {
+                MessageBox.Show("error input");
+                return;
+            }
+
+            hourlyRate = Convert.ToDouble(tbHourlyRatePay.Text);
+            hours = Convert.ToDouble(tbNoOfHoursPay.Text);
+
+            double Gross = (hourlyRate * hours);
+            double FedTax = (Gross * 0.075);
+            double ProvTax = Gross * 0.075;
+            double NetPay = Gross - (FedTax + ProvTax);
+
+            tbTotalGrossPay.Text = Gross.ToString();
+            tbTotalNetPay.Text = NetPay.ToString();
+            tbFederalTaxPay.Text = FedTax.ToString();
+            tbProvincialTaxPay.Text = ProvTax.ToString();
+
+
+
+        }
+
+        private void btnSavePayment_Click(object sender, RoutedEventArgs e)
+        {
+
+            //collect info from the text boxs:            
+
+            int id = Convert.ToInt32(tbEmployeeID.Text);
+
+            double hourlyRate;
+            if (!double.TryParse(tbHourlyRatePay.Text, out hourlyRate))
+            {
+                MessageBox.Show("error input");
+                return;
+            }
+            double hours;
+            if (!double.TryParse(tbNoOfHoursPay.Text, out hours))
+            {
+                MessageBox.Show("error input");
+                return;
+            }
+
+            hourlyRate = Convert.ToDouble(tbHourlyRatePay.Text);            
+            hours = Convert.ToDouble(tbNoOfHoursPay.Text);
+
+            double net = Convert.ToDouble(tbTotalNetPay.Text);
+
+            
+            Payment payment = new Payment();
+            payment.Id = id;
+            payment.HourlyRate = hourlyRate;
+            payment.NetPay = net;
+            payment.Hours = hours;
+
+            try
+            {
+                db = new ProjectDatabase();
+                db.AddPayment(payment);
+                MessageBox.Show("The Payment record was successfully saved .");
+                this.Close();
+            }
+            catch (SqlException ex)
+            {
+                //MessageBox.Show("Check the Database Connection Please.");
+                MessageBox.Show(ex.ToString());
+            }
+            
+
+            
+        }
+    }
+}
---
+++ HR/HR/UpdateEmployee.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HR
+{
+    /// <summary>
+    /// Interaction logic for UpdateEmployee.xaml
+    /// </summary>
+    public partial class UpdateEmployee : Window
+    {
+        public UpdateEmployee()
+        {
+            InitializeComponent();
+        }
+
+        //private void btnAddNew_Click(object sender, RoutedEventArgs e)
+        //{
+        //    UpdateEmployee edit = new UpdateEmployee();
+        //    edit.SaveEvent += new UpdateEmployee.SaveEventHandler(edit_SaveEvent);  //Add event handler
+        //    edit.name = Employee[this.lsvEmployee.SelectedItems[0].Index].Name;
+        //    edit.age = Employee[this.lsvEmployee.SelectedItems[0].Index].Age;
+        //    edit.ShowDialog(this);
+        //}
+    }
+}
